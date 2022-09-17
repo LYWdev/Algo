12e650dcd9f82314d285f13e754d71b1ed49a9e5
LYWdev <yoonwoodev@kakao.com>
2022-09-17 16:18:50 +0900

    문제하결 완료

--- cotest/test1.c
+++ cotest/test1.c
@@ -1,27 +1,34 @@
+#include<stdio.h>
+int div(int n, int i);
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-/*
-    자연수 n이 매개변수로 주어집니다.
-    n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return
-    하도록 solution 함수를 완성해주세요.
-    답이 항상 존재함은 증명될 수 있습니다.
-*/
 int solution(int n)
 {
     int i = 1;
     int answer = 0;
-    div(n, i);
+    //재귀 함수 시작
+    answer = div(n, i);
+    return answer;
 }
+
 int div(int n, int i)
 {
+    // n% 1 문제조건 만족 리턴 
     if (n % i == 1)
     {
         return i;
     }
+
     else
     {
-        return (div(n, i++))
+        //아니면 다시 재귀로 호출
+        return(div(n, ++i));
     }
+}
+
+int main()
+{
+    int n[5] = { 1, 12, 8, 34, 99};
+    int i;
+    for(i=0; i < 5; i++)  
+        printf("%d일때  : %d\n", n[i], solution(n[i]));
 }
